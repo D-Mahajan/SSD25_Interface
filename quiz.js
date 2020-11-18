7440142259cc37c576ecd29865df8ab2ed8be5c1
Abhisek-Mohapatra <71816158+Abhisek-Mohapatra@users.noreply.github.com>
2020-11-18 12:01:40 +0530

    Quiz js file

---
+++ quiz.js
@@ -0,0 +1,40 @@
+var Questions = { 
+    "qArray": [{
+            "question": "Which of the following is not a Shell ?",
+            "option1": "Bat",
+            "option2": "Ksh",
+            "option3": "Bourne",
+            "option4": "Bash",
+            "correctAns": "Bat"
+        },
+
+        {
+            "question": "How to display all array elements at once?",
+            "option1": "echo ${array[@]}",
+            "option2": "echo ${array[#]}",
+            "option3": "echo ${array[]}",
+            "option4": "echo array",
+            "correctAns": "echo ${array[@]}"
+        },
+
+        {
+            "question": "How to define functions in shell scripting?",
+            "option1": "int function test(){ Commands return result; }",
+            "option2": "Begin function test Commands End;",
+            "option3": "function test(){ Commands }",
+            "option4": "start test(){ Commands return $TRUE }",
+            "correctAns": "function test(){ Commands }"
+        },
+
+        {
+            "question": "How to get input from the terminal for shell script?",
+            "option1": "read command",
+            "option2": "scan command",
+            "option3": "None",
+            "option4": "input command",
+            "correctAns": "read command"
+        },
+
+       
+    ]
+};
