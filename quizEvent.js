7afea7d3fb5e8794d5b27975670119259f462510
Abhisek-Mohapatra <71816158+Abhisek-Mohapatra@users.noreply.github.com>
2020-11-18 13:30:36 +0530

    Add files via upload

--- quizEvent.js
+++ quizEvent.js
@@ -38,7 +38,7 @@ $(document).ready(function() { // When quiz page is loaded   Nov 10 6:15 pm
     var j = 0;
     var qArr = [];
 
-    for (j = 0; j < 10; j++) {
+    for (j = 0; j < 15; j++) {
         qArr.push(j);
     }
 
@@ -90,7 +90,110 @@ $(document).ready(function() { // When quiz page is loaded   Nov 10 6:15 pm
 
 
 
-  
+    var i = 0;
+
+
+    for (j = 0; j < 10; j++) {
+
+        var qContent = Questions.qArray[qArr[j]];
+
+        // console.log("Hello");
+
+        var qElement = document.createElement("div");
+
+        qElement.setAttribute("id", "qq");
+
+        var q = qContent.question;
+
+        $("#qq").addClass("qheader");
+        qElement.innerHTML = (j + 1) + ") " + q;
+
+        $(".qheader").append('</div>');
+
+        $("#formQuiz").append(qElement);
+
+
+        var options = ['a)', 'b)', 'c)', 'd)'];
+        for (i = 1; i <= 4; i++) {
+            //  var choice = "selectedQuestion.choice" + j;
+            var txt = "qContent.option" + i;
+            //   console.log(eval(txt));
+            var radioBtn = $('<input type="radio" name="q' + qArr[j] + '" id="q' + qArr[j] + i + '" value="' + eval(txt) + '">');
+            //  radioBtn.text = eval("qContent.option" + i);
+            // $("#q1" + i).text("Hey");
+
+            //  console.log("name=q" + qArr[j]);
+
+            radioBtn.appendTo('#formQuiz');
+            $('#formQuiz').append('<label>' + options[i - 1] + ' ' + eval(txt) + ' </label>');
+            $("#formQuiz").append('<br>');
+            //  console.log("#q1" + i);
+            // console.log(eval(txt));
+            $("label").css("font-weight", "normal");
+
+            //  console.log("Checked inside loop are:" + $('input[id="q01"]').val());
+        }
+        $("#formQuiz").append('<br>');
+        //  console.log(eval("qContent.option" + 1));
+
+        //   $(".btn-lg1").click(function() { 
+
+    }
+
+
+    // console.log("Val is" + $('input[name=q' + 0 + ']:checked').val());
+
+    $("#checkAns").click(function() {
+
+
+        //  console.log("Checked are:" + $('input[name="q' + 0 + ']').val());
+        //  console.log("Checked are:" + $("#q01").val());
+
+
+        var totalMarks = 0;
+        var recordedAns;
+        var qContent1;
+        var k = 0;
+        for (k = 0; k < qArr.length; k++) {
+            //  console.log(qArr[k]);
+            // console.log("Hi q" + qArr[k] + "Hm" + $('input[name="q' + qArr[k] + '"]').length);
+            if ($('input[name=q' + qArr[k] + ']:checked').length > 0) {
+                // do something here
+                //   recordedAns = $.trim($('input[name=q' + qArr[k] + ']:checked').val()).replace(' ', '+');
+                recordedAns = $('input[name=q' + qArr[k] + ']:checked').val();
+                //  console.log(recordedAns);
+
+                qContent1 = Questions.qArray[qArr[k]];
+                //  console.log(qContent1.correctAns);
+
+
+
+                if (recordedAns == qContent1.correctAns) {
+                    totalMarks++;
+                }
+
+            }
+
+            $('input[name=q' + qArr[k] + ']').prop('checked', false);
+
+
+        }
+
+        swal({
+            title: "Total Marks obtained : ",
+            text: totalMarks + "/10",
+            //icon: "info",
+        });
+
+       // alert("Total Marks obtained are: " + totalMarks);
+
+
+
+
+
+
+    });
+
 
 
 
